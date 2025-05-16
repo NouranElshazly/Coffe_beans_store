a72f6e47b2df8ed530049eebcc9dd38f245c698d
Nouran <112038299+NouranElshazly@users.noreply.github.com>
2025-05-16 22:49:37 +0300

    Update settings.py

--- CoffeShop/settings.py
+++ CoffeShop/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-+1+y*c=m1eh6p4qa14st2pd(l^e$idmay6(-i=2)!tbkf7vlyj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['*']
+ 
 
-
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
